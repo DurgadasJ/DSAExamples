e7ee037e9d6d27aab4c4d4de02a28855885f6199
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2024-09-28 11:01:14 +0530

    Code 11 Commit

--- src/recursion/ValidPalindrome.java
+++ src/recursion/ValidPalindrome.java
@@ -3,6 +3,28 @@ package recursion;
 //https://leetcode.com/problems/valid-palindrome/description/
 public class ValidPalindrome {
     public static void main(String[] args) {
+        String str  = "A man, a plan, a canal: Panama";
+        boolean bool = checkPalindrome(str);
+        System.out.println(bool);
+    }
 
+    private static boolean checkPalindrome(String str) {
+        int left =0,right = str.length()-1;
+
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(str.charAt(left))){
+                left++;
+            }
+            while(left<right && !Character.isLetterOrDigit(str.charAt(right))){
+                right--;
+            }
+            if(Character.toLowerCase(str.charAt(left))!=Character.toLowerCase(str.charAt(right))){
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
     }
 }
