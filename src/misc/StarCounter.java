c58fcadc40fa2e5937b6f6c57789a8d9862e6b96
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2024-10-13 00:53:46 +0530

    Code 14 Commit

--- src/misc/StarCounter.java
+++ src/misc/StarCounter.java
@@ -2,18 +2,31 @@ package misc;
 
 public class StarCounter {
     public static void main(String[] args) {
-        int n =3;
-        //1 3 5 7 9 11 13 15
+        int n = 5;
 
-        int sum = 2*n -1;
-        for(int i=0;i<n;i++){
-            for(int l=0;l<i;l++){
+        for (int i = 0; i < n; i++) {
+            for (int j = n - i; j > 1; j--) {
                 System.out.print("  ");
             }
-            for(int j=0;j<sum;j++){
-                System.out.print("* ");
+            for (int j = 0; j <= i * 2; j++) {
+                System.out.print("*");
+                if (j < i * 2) {
+                    System.out.print(" ");
+                }
             }
-            sum -=2;
+            System.out.println();
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = n - i; j > 0; j--) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+                if (j < i * 2 - 2) {
+                    System.out.print(" ");
+                }
+            }
             System.out.println();
         }
     }
