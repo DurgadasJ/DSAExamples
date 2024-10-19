3f98926688d2ccb4f53b1caa84799a1096dd08ab
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2024-10-19 19:24:46 +0530

    Code 18 Commit

---
+++ src/arrays/RotateArray.java
@@ -0,0 +1,49 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int d = 2;
+        rotateArray_1(arr.clone(),d);
+        rotateArray_2(arr,d);
+    }
+    public static void rotateArray_1(int[] arr,int d){
+        int n = arr.length;
+
+        d %= n;
+
+        int[] temp = new int[n];
+
+        for (int i = 0; i < n - d; i++)
+            temp[i] = arr[d + i];
+
+        for (int i = 0; i < d; i++)
+            temp[n - d + i] = arr[i];
+
+        for (int i = 0; i < n; i++)
+            arr[i] = temp[i];
+        System.out.println("Result 1 :" +Arrays.toString(arr));
+    }
+    private static void rotateArray_2(int[] arr, int d) {
+        int n = arr.length;
+        d %= n;
+
+        revArray(arr,0,d-1);
+        revArray(arr,d,n-1);
+        revArray(arr,0,n-1);
+        System.out.println("Result 2 :" +Arrays.toString(arr));
+    }
+
+    private static void revArray(int[] arr, int i, int i1) {
+        int left =i , right = i1;
+        while(left<right){
+            int tmp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = tmp;
+            left++;
+            right--;
+        }
+    }
+}
