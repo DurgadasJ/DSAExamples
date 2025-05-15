bc8797035187893ce1cfeb1563783cd67e96f2cb
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-05-15 12:23:42 +0530

    Tree Code 22 Commit

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,6 +1,7 @@
 package arrays;
 
 import java.util.HashMap;
+import java.util.Map;
 
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
@@ -8,7 +9,7 @@ public class TwoSum {
     public static void main(String[] args) {
         int[] nums = {2,11,7,15};
         int target = 9;
-        int[] result = twoSum(nums, target);
+        int[] result = twoSum_1(nums, target);
         if (result.length == 0) {
             System.out.println("No two sum solution found.");
         } else {
@@ -16,17 +17,32 @@ public class TwoSum {
         }
     }
 
-    private static int[] twoSum(int[] nums, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            int diff = target - nums[i];
-            if(map.containsKey(diff)){
-                return new int[]{map.get(diff),i};
+    private static int[] twoSum_1(int [] arr,int target){
+        Map<Integer,Integer> map = new HashMap<>();
+
+        for(int i=-0;i<arr.length;i++){
+            int rem = target - arr[i];
+            if(map.containsKey(rem)){
+                return new int[]{map.get(rem),i};
             }else{
-                map.put(nums[i],i);
+                map.put(arr[i],i);
             }
         }
 
-        return new int[]{};
+        return new int []{};
     }
+
+//    private static int[] twoSum(int[] nums, int target) {
+//        HashMap<Integer,Integer> map = new HashMap<>();
+//        for(int i=0;i<nums.length;i++){
+//            int diff = target - nums[i];
+//            if(map.containsKey(diff)){
+//                return new int[]{map.get(diff),i};
+//            }else{
+//                map.put(nums[i],i);
+//            }
+//        }
+//
+//        return new int[]{};
+//    }
 }
