2aa328d76161a54cb7d7876bf1e0ee6dee4c49c5
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-06-22 20:12:32 +0530

    DSA

--- src/arrays/MakeUniqueArray.java
+++ src/arrays/MakeUniqueArray.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class MakeUniqueArray {
     public static void main(String[] args) {
-        int [] arr =  {1,6,3,4,3,4,9,2,7,8,9,10,11};
+        int [] arr =  {1,6,3,4,3,4,9,2,7,8,9,10,11,6,45,3,2,45,767,879,111,345};
         int [] arr_1 = uniqueArray(arr);
         System.out.println(Arrays.toString(arr_1));
     }
@@ -20,6 +20,9 @@ public class MakeUniqueArray {
         }
 
         int[] uniqueArray = set.stream().mapToInt(Integer::intValue).toArray();
+        Integer[] uni2 = set.toArray(new Integer[0]);
+
+        System.out.println(Arrays.toString(uni2));
         return uniqueArray;
     }
 }
