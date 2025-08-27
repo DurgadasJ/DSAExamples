b7d73427b6f9a534dfc3f1109d7fb72b53ca3cea
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-08-27 12:02:09 +0530

    Code Details Updated

--- src/recursion/ReverseArray.java
+++ src/recursion/ReverseArray.java
@@ -2,22 +2,25 @@ package recursion;
 
 import java.util.Arrays;
 
+// Reverse an array using recursion
 public class ReverseArray {
     public static void main(String[] args) {
         int [] arr = {7,5,2,4,6};
-
+        // reverse array using recursion
         int [] arr2 = reverseArray(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr2));
     }
     static int[] reverseArray(int arr[],int left,int right){
+        // base case
         if (left >= right) {
             return arr;
         }
 
+        // swap
         int tmp = arr[left];
         arr[left] = arr[right];
         arr[right] = tmp;
-
+        // recursive call
         return reverseArray(arr,left+1,right-1);
     }
 }
