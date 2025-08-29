16d9586883bbf5d4d75b5540c8f68b9296a6df0c
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-08-29 17:23:53 +0530

    Code Details Updated

--- src/priorityQueue/KtheLargestEle.java
+++ src/priorityQueue/KtheLargestEle.java
@@ -2,6 +2,7 @@ package priorityQueue;
 
 import java.util.PriorityQueue;
 
+// Kth largest element in an array
 public class KtheLargestEle {
     public static void main(String[] args) {
         int []nums = {3,2,1,5,6,4};
@@ -12,11 +13,14 @@ public class KtheLargestEle {
     }
 
     private static int findKthLargest(int[] nums, int k) {
+        // minHeap to store the k largest elements
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
+        // add elements to the minHeap
         for(int num:nums){
             minHeap.offer(num);
+            // keep the size of the minHeap to k
             if(minHeap.size()>k){
+                // remove the smallest element
                 minHeap.poll();
             }
         }
