22d181d3d538ee644f40ba10de7d8f0e82fda1a2
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-08-31 20:16:34 +0530

    Code Details Updated

--- src/misc/ReverseNumber.java
+++ src/misc/ReverseNumber.java
@@ -1,11 +1,14 @@
 package misc;
 
+//  Java program to reverse a number
 public class ReverseNumber {
     public static void main(String[] args) {
         int num =48953;
         int revnum=0;
+        // logic to reverse a number
         while(num>0){
             int rem = num % 10;
+            // build the reversed number
             revnum = revnum*10+rem;
             num = num /10;
         }
