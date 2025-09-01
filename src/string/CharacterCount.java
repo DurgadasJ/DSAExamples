f65f7249a5f19669d90a284e767a24fe418a84f9
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-09-01 22:35:40 +0530

    Code Details Updated

--- src/string/CharacterCount.java
+++ src/string/CharacterCount.java
@@ -1,12 +1,23 @@
 package string;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class CharacterCount {
     public static void main(String[] args) {
         String str1 = "Welcome to Intellij Ideaa";
         String str = str1.toLowerCase();
-
+        //how to remove spaces from string
+        str.replace(" ","");
+        // .replace() doesn't change the original string as strings are immutable in java
+        // so we need to assign it to a new string or the same string variable
+        str1 = "Welcome to Java Programming Language";
+        // str1 will have the updated string now
+        System.out.println(str1);
+        System.out.println(str1.concat("!!!"));
+        //str1 will not change as concat also doesn't change the original string
+        // we need to assign it to a new string or the same string variable
+        // System.out.println(str1.concat("!!!")); this will print the output with !!!
         HashMap<Character,Integer> map = new HashMap<>();
 
         for(int i=0;i<str.length();i++){
@@ -20,18 +31,27 @@ public class CharacterCount {
                 map.put(ch,1);
             }
         }
-        System.out.println(map.toString());
-        System.out.println(map);
+        System.out.println(map.toString()); // prints the map in key-value pairs
+        System.out.println(map);// prints the map in key-value pairs
 
+        // Map is an interface and HashMap is a class that implements the Map interface
+        // here we are using Map interface to declare the map variable
+        // and HashMap class to create the object of the map
+        // This is done to achieve abstraction and loose coupling
+        // We can change the implementation of the map to TreeMap or LinkedHashMap without changing the code
+        // as long as we are using the Map interface to declare the map variable
+        // This is a good practice to follow
         Map<Character,Integer> map1 = new HashMap<>();
-
+        // iterate through each character of string
         for(char ch : str.toCharArray()){
-            if(ch!=''){
+            if(ch!= ' '){
+                // If character is already present in map, increment its count by 1
+                // If character is not present in map, add it to map with count 1
                 map1.put(ch,map1.getOrDefault(ch,0)+1);
             }
         }
-
-        for(Entry<Character,Integer> entry: map1.entrySet()){
+        // print the map in key-value pairs using entry set
+        for(Map.Entry<Character,Integer> entry: map1.entrySet()){
             System.out.println(entry.getKey()+", "+entry.getValue());
         }
     }
