fc1ee904479f3035401310c23748241e6001640d
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-09-12 23:46:16 +0530

    code 29

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,6 +1,8 @@
 package arrays;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 //Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -19,9 +21,11 @@ public class TwoSum {
 
     private static int[] twoSum_1(int [] arr,int target){
         Map<Integer,Integer> map = new HashMap<>();
-
-        for(int i=-0;i<arr.length;i++){
+        // traverse / iterate the array
+        for(int i=0;i<arr.length;i++){
+            // find the complement
             int rem = target - arr[i];
+            // check if rem exists in map
             if(map.containsKey(rem)){
                 return new int[]{map.get(rem),i};
             }else{
@@ -31,6 +35,24 @@ public class TwoSum {
 
         return new int []{};
     }
+
+    //to return list of pair having two sum
+    private static List<List<Integer>> twoSumPairs(int [] arr, int target){
+        Map<Integer,Integer> map = new HashMap<>();
+        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result1  = new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            int rem = target - arr[i];
+            if(map.containsKey(rem)){
+                  result.add(java.util.Arrays.asList(map.get(rem),i));
+                  result1.add(new ArrayList<>(List.of(map.get(rem),i)));
+            }else{
+                  map.put(arr[i],i);
+            }
+        }
+        return result;
+    }
+
 
 //    private static int[] twoSum(int[] nums, int target) {
 //        HashMap<Integer,Integer> map = new HashMap<>();
