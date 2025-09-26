60d0cdfa9f58183e512c6de048e2ae0c86b5a870
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-09-27 05:26:13 +0530

    modified

--- src/streams/WUQuestion.java
+++ src/streams/WUQuestion.java
@@ -1,10 +1,14 @@
 package streams;
 
+import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class WUQuestion {
     public static void main(String[] args) {
         String str = "aaaabBbCCDd";
+
+
 
         str.chars().mapToObj(c->(char) c)
                 .collect(Collectors.groupingBy(c->c,Collectors.counting()))
@@ -17,7 +21,33 @@ public class WUQuestion {
                 .collect(Collectors.groupingBy(c -> c,Collectors.counting()))
                 .forEach((k, v) -> System.out.print(k.toString() + v));
 
+        System.out.println();
+        String str1 = "sasdsd";
+        Map<Character, Long> map =str1.chars()
+                .mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(c->c,Collectors.counting()));
 
+        for(char ch: str1.toCharArray()){
+            if(map.get(ch)==1){
+                System.out.println(ch);
+                break;
+            }
+        }
+        String str2 = "zsdsdsdsa";
+//        Map<Character, Long> map1 =
+
+        System.out.println(str2.chars()
+                .mapToObj(c -> (char)c)
+                .collect(Collectors.groupingBy(c->c, LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream()
+                .filter(e->e.getValue()==1).findFirst());
+
+//        for (char ch : str2.toCharArray()) {
+//            if (map1.get(ch) == 1) {
+//                System.out.println(ch);
+//                break;
+//            }
+//        }
 //        str.chars().mapToObj(c->(char)c).collect(Comparable.class);
     }
 }
