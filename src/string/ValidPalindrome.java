70aacb5dbf8242b2638cdc864f9ad422454a03b5
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-10-07 20:27:10 +0530

    Another Approach Modified

--- src/string/ValidPalindrome.java
+++ src/string/ValidPalindrome.java
@@ -6,6 +6,30 @@ public class ValidPalindrome {
         String str  = "A man, a plan, a canal: Panama";
         boolean bool = checkPalindrome(str);
         System.out.println(bool);
+
+         StringBuilder sb = new StringBuilder();
+         for(int i=0;i<str.length();i++){
+             char ch = str.charAt(i);
+             if(isAlphaNumeric(ch)){
+                 sb.append(toLowerCase(ch));
+             }
+         }
+         String filteredStr = sb.toString();
+         boolean isPalin = isPalindrome(filteredStr);
+         System.out.println(isPalin);
+
+    }
+
+    private static boolean isAlphaNumeric(char ch){
+        return (ch>='a' && ch<='z') || (ch>='A' && ch<='Z') || (ch>='0' && ch<='9');
+    }
+
+    private static char toLowerCase(char ch){
+        if(ch>='a' && ch<='z'){
+            return ch;
+        }else{
+            return (char)(ch - 'A' + 'a');
+        }
     }
 
     private static boolean checkPalindrome(String str) {
