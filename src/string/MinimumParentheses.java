87c8f0b8d7660f44ebd73a34c59a39fcea8f74a5
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-10-08 10:24:49 +0530

    modified

--- src/string/MinimumParentheses.java
+++ src/string/MinimumParentheses.java
@@ -7,6 +7,8 @@ public class MinimumParentheses {
         System.out.println(count);
     }
 
+
+
     private static int mincount(String str) {
         int n= str.length(), lcount=0,rcount=0,added=0;
         for(int i=0;i<n;i++){
