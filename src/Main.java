c04f65b54fb00ac23aa7588f5947398be432a61c
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-10-13 18:40:20 +0530

    modified file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Project contains dsa examples!");
 	// Project contains dsa examples
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
