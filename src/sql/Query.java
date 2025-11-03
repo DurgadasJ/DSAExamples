e4a7277c60083dc92079eb3895e7f38ebe2a5de0
DurgadasJ <durgadas.vijay.joshi@gmail.com>
2025-11-03 23:29:57 +0530

    modified file

--- src/sql/Query.java
+++ src/sql/Query.java
@@ -13,6 +13,7 @@ public class Query {
                                     "group by d.dept_name having count(e.id) > 5";
 
 
+
         String deleteDuplEmail = "delete p1 from person p1 " +
                                  "join person p2 on p1.email = p2.email" +
                                  "where p1.id > p2.id";
